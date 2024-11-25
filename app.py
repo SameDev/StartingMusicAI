3a549060f2e4eecec0e4c4d63a7a5e6828f1f173
SameDev <conradtsamuel@gmail.com>
2024-11-25 20:04:54 -0300

    format, and name correction

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import pandas as pd
 import requests
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.feature_extraction.text import TfidfVectorizer
-import json
 
 app = Flask(__name__)
 
 # Configuração do logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# URLs das APIs
+users_api_url = "https://starting-music.onrender.com/user"
+songs_api_url = "https://starting-music.onrender.com/music"
 
 # Função para obter dados das APIs
 def get_data(url):
@@ -23,16 +25,11 @@ def get_data(url):
         logging.error("Erro ao obter dados da URL %s: %s", url, e)
         return {}
 
-# URLs das APIs
-users_api_url = "https://starting-music.onrender.com/user"
-songs_api_url = "https://starting-music.onrender.com/music"
-
 # Função para extrair o nome de itens em uma lista de dicionários
 def extract_name(item_list):
-    return [item['name'] for item in item_list] if isinstance(item_list, list) else []
+    return [item['nome'] for item in item_list] if isinstance(item_list, list) else []
 
 # Função para carregar e processar os dados
-# Carrega os dados dos usuários e músicas a partir das APIs e os processa
 def load_data():
     try:
         users_data = get_data(users_api_url)
@@ -42,7 +39,7 @@ def load_data():
         users_df = pd.DataFrame(users_data.get('user', []))
         songs_df = pd.DataFrame(songs_data.get('songs', []))
 
-        # Normaliza e processa os dados dos usuários
+        # Processar dados dos usuários
         if 'tags' in users_df.columns:
             users_df_normalized = pd.json_normalize(users_df['tags'])
             users_df = pd.concat([users_df.drop(columns=['tags']), users_df_normalized], axis=1)
@@ -53,7 +50,7 @@ def load_data():
         if 'playlist' in users_df.columns:
             users_df['playlist'] = users_df['playlist'].apply(lambda x: extract_name(x))
 
-        # Normaliza e processa os dados das músicas
+        # Processar dados das músicas
         if 'tags' in songs_df.columns:
             songs_df_normalized = pd.json_normalize(songs_df['tags'])
             songs_df = pd.concat([songs_df.drop(columns=['tags']), songs_df_normalized], axis=1)
@@ -73,7 +70,6 @@ def load_data():
         return pd.DataFrame(), pd.DataFrame()
 
 # Função para garantir que todos os valores são strings
-# Isso evita problemas com tipos de dados mistos
 def ensure_strings(data):
     if isinstance(data, dict):
         return {str(k): ensure_strings(v) for k, v in data.items()}
@@ -85,9 +81,24 @@ def ensure_strings(data):
         return str(data)
     return data
 
+# Função para formatar as músicas no formato desejado
+def format_song(song):
+    return {
+        "id": song.get("id"),
+        "nome": song.get("nome"),
+        "artista": song.get("artist", ""),
+        "url": song.get("url", ""),
+        "duracao": song.get("duracao", ""),
+        "data_lanc": song.get("data_lanc", ""),
+        "image_url": song.get("image_url", ""),
+        "albumId": song.get("albumId", ""),
+        "tags": [{"id": tag.get("id"), "nome": tag.get("nome")} for tag in song.get("tags", [])],
+        "artistaId": song.get("artistaId", []),
+        "playlist": song.get("playlist", []),
+        "usuarioGostou": song.get("userLiked", [])
+    }
+
 # Função para recomendar músicas
-# Essa função utiliza o ID do usuário para buscar as músicas que ele gostou
-# e recomenda músicas semelhantes com base no conteúdo e popularidade
 def recommend_songs(user_id, users_df, songs_df):
     try:
         user_id = int(user_id)
@@ -95,49 +106,34 @@ def recommend_songs(user_id, users_df, songs_df):
         return {"error": "User ID must be an integer"}
 
     user_data = users_df[users_df['id'] == user_id]
-    
     if user_data.empty:
         return {"error": "User not found"}
 
-    # Verifica se o usuário curtiu alguma música
     liked_list = user_data['gostei'].values[0]
     if not liked_list:
-        # Se não tiver curtido nenhuma música, recomenda as 10 músicas mais populares ou recentes
-        recommendations = songs_df.head(10).to_dict(orient='records')
-        return {"songs": ensure_strings(recommendations)}
-    
+        recommendations = songs_df.head(10).apply(format_song, axis=1).tolist()
+        return {"songs": recommendations}
+
     liked_songs = set(liked_list)
-    songs_to_recommend = set(songs_df['name']) - liked_songs
+    songs_to_recommend = set(songs_df['nome']) - liked_songs
 
-    songs_df['information'] = songs_df['name'] + ' ' + songs_df['artist'] + ' ' + songs_df['playlist'].apply(lambda x: ' '.join(x))
-
-    # Aplicar a vetorização TF-IDF nas informações das músicas
+    songs_df['information'] = songs_df['nome'] + ' ' + songs_df['artist'] + ' ' + songs_df['playlist'].apply(lambda x: ' '.join(x))
     tfidf_vectorizer = TfidfVectorizer(stop_words='english')
     tfidf_matrix = tfidf_vectorizer.fit_transform(songs_df['information'])
-
-    # Calcula a similaridade de cosseno entre as músicas
     cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
 
     similar_songs = {}
     for i, row in enumerate(cosine_similarities):
         similar_songs_list = [songs_df.iloc[j] for j in row.argsort()[-6:-1][::-1] if i != j]
-        similar_songs[songs_df.iloc[i]['name']] = similar_songs_list
+        similar_songs[songs_df.iloc[i]['nome']] = similar_songs_list
 
     recommendations = []
     for song in songs_to_recommend:
         if song in similar_songs:
             recommendations.extend(similar_songs[song])
-    
-    # Limita o número de recomendações a 10
+
     recommendations = recommendations[:10]
-    recommendations = [song.to_dict() for song in recommendations]
-
-    # Garantir que todos os valores são strings
-    recommendations = [ensure_strings(song) for song in recommendations]
-
-    # Verifica o tipo e conteúdo das recomendações
-    logging.debug("Tipo de recommendations: %s", type(recommendations))
-    logging.debug("Conteúdo de recommendations: %s", recommendations)
+    recommendations = [format_song(song) for song in recommendations]
 
     return {"songs": recommendations}
 
@@ -153,16 +149,7 @@ def recommend():
         return jsonify({"error": "Error loading data"}), 500
 
     recommendations = recommend_songs(user_id, users_df, songs_df)
-
-    # Verifica o tipo de dados retornados
-    if not isinstance(recommendations, dict):
-        return jsonify({"error": "Returned data is not in the correct format"}), 500
-
-    # Loga o tipo e conteúdo da resposta antes de enviar
-    logging.debug("Final response: %s", recommendations)
-
     return jsonify(ensure_strings(recommendations))
-
 
 if __name__ == '__main__':
     app.run()
